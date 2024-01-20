c293dd00322059ed1d94491024e6b2f5eff92bfa
Sunil-B-K <sunilkuligod21@gmail.com>
2024-01-20 12:40:54 +0530

    coverpage fixed

--- src/Components/MyBook.jsx
+++ src/Components/MyBook.jsx
@@ -1,6 +1,6 @@
 import HTMLFlipBook from "react-pageflip";
 import Page from "./Page";
-import Cover from "./pages/Cover/Cover.jsx";
+import Cover from "./Pages/Cover/Cover.jsx";
 import coverImg from "../assets/cover.png";
 import EndImg from "../assets/last-page.png";
 
