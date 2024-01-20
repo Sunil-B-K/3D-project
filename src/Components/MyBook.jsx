0a92be9fe4edc7ab02331775edb73f9bd0f798fc
Sunil-B-K <sunilkuligod21@gmail.com>
2024-01-20 12:54:46 +0530

    secondpage aded

--- src/Components/MyBook.jsx
+++ src/Components/MyBook.jsx
@@ -3,7 +3,7 @@ import Page from "./Page";
 
 import coverImg from "../assets/cover.png";
 import EndImg from "../assets/last-page.png";
-import Cover from "./Pages/Cover/Cover.jsx";
+// import Cover from "./Pages/Cover/Cover.jsx";
 
 import FirstPage from "./Pages/Hero/FirstPage.jsx";
 import Skills from "./pages/Skills/Skills.jsx";
@@ -15,6 +15,7 @@ import ProjectImg1 from "../assets/projects/car-rental.jpg";
 import ProjectImg2 from "../assets/projects/restaurant.jpg";
 import ProjectImg3 from "../assets/projects/ecommerce.jpg";
 import ProjectImg4 from "../assets/projects/travel-website.jpg";
+import Second from "./Second/Second.jsx";
 // import Contact from "./Contact/Contact.jsx";
 
 
@@ -54,7 +55,7 @@ const ProjectData = [
     return (
         <HTMLFlipBook width={600} height={700} showCover={true}>
             <Page number={1}>
-                <Cover coverImg={coverImg} title="MyPortfolio" />
+                <Second coverImg={coverImg} title="MyPortfolio" />
             </Page>
             <Page number={2}><FirstPage /></Page>
             <Page number={3}><Skills /></Page>
@@ -62,7 +63,7 @@ const ProjectData = [
             <Page number={5}><About /></Page>
             <Page number={6}><Projects ProjectData={ProjectData} /></Page>
             <Page number={7}><Projects ProjectData={ProjectData2} /></Page>
-            <Page number={8}><Cover coverImg={EndImg} title="Thanks" /></Page>
+            <Page number={8}><Second coverImg={EndImg} title="Thanks" /></Page>
         </HTMLFlipBook>
     );
 }
